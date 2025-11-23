7d05c2434a1b4b1c35d65f865dc041f4865e875e
vejnar <charles.vejnar@gmail.com>
2025-11-23 18:13:03 -0500

    Copy packages to channel dir

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 rattler-build build --recipe-dir recipes --skip-existing=all --package-format conda:19 --output-dir build
 zstd -19 --force build/*/repodata.json
-rsync -vaP build/linux-64 build/noarch repo/
+rsync -vaP build/linux-64 build/noarch channel/
