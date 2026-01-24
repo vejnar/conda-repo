471fb6c57db2cbe2e7cdfc19a8e08a1482fea55d
vejnar <charles.vejnar@gmail.com>
2026-01-24 14:02:37 -0500

    Switch to building within container

--- build.sh
+++ build.sh
@@ -2,6 +2,30 @@
 
 set -e
 
-rattler-build build --recipe-dir recipes --skip-existing=all --package-format conda:19 --output-dir build
+mkdir -p bin
+
+# Get rattler-build
+if [ ! -f "bin/rattler-build" ] ; then
+  wget --timestamping -O bin/rattler-build https://github.com/prefix-dev/rattler-build/releases/latest/download/rattler-build-x86_64-unknown-linux-musl
+  chmod +x bin/rattler-build
+fi
+
+# Get patchelf
+if [ ! -f "bin/patchelf" ] ; then
+  wget --timestamping https://github.com/NixOS/patchelf/releases/download/0.18.0/patchelf-0.18.0-x86_64.tar.gz
+  tar xvfz patchelf-0.18.0-x86_64.tar.gz ./bin/patchelf
+  rm -f patchelf-0.18.0-x86_64.tar.gz
+fi
+
+apptainer exec \
+  --bind ./bin/rattler-build:/usr/bin/rattler-build \
+  --bind ./bin/patchelf:/usr/bin/patchelf \
+  docker://condaforge/linux-anvil-x86_64:alma8 \
+  rattler-build build \
+    --recipe-dir recipes \
+    --skip-existing=all \
+    --package-format conda:19 \
+    --output-dir build
+
 zstd -19 --force build/*/repodata.json
 rsync -vaP build/linux-64 build/noarch channel/
